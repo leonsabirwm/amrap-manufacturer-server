dee0ad8aea91d3dc95a7f513980d3e11b6e0e89e
leon-sabir <leonsabirwm@gmail.com>
2022-05-25 02:29:41 +0600

    admin requests are seqeured

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:"Unauthorized Access"});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded) {
+        if(error){
+            return res.status(403).send({message:"Forbidden Access"});
+        }
+        req.decoded = decoded;
+        next();
+      });
 
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9rrhg.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,19 +41,27 @@ async function run() {
     const orderCollection = client.db('amrap').collection('orders');
     const reviewCollection = client.db('amrap').collection('reviews');
 
-
+    const verifyAdmin =async (req,res,next)=>{
+        const requester = req.decoded.email;
+        const filter = {email : requester};
+        const account = await userCollection.findOne(filter);
+        if(account.role === "admin"){
+            next();
+        }
+        else{
+            res.status(403).send({message:"forbidden"});
+        }
+    }
     app.post('/create-payment-intent',async(req,res)=>{
         const order = req.body;
         const price = order.price;
-       if(price){
+        if(price){
         const amount = (price*100);
-        console.log('order is',amount);
         const paymentIntent = await stripe.paymentIntents.create({
             amount : amount,
             currency: 'bdt',
             payment_method_types:['card']
           });
-        console.log('hey')
         console.log(paymentIntent.client_secret);
           res.send({clientSecret: paymentIntent.client_secret})
        }
@@ -56,7 +78,6 @@ async function run() {
         else{
             res.send({admin:false});
         }
-        console.log(user);
     })
     app.post("/parts",async(req,res)=>{
         const product = req.body;
@@ -70,7 +91,7 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
-    app.delete("/parts/:id",async(req,res)=>{
+    app.delete("/parts/:id",verifyJWT,verifyAdmin,async(req,res)=>{
         const id = req.params.id;
         const filter = {_id : ObjectId(id)};
         const result = await partCollection.deleteOne(filter);
@@ -104,8 +125,7 @@ async function run() {
         const result = await orderCollection.findOne(filter);
         res.send(result);
     });
-    app.patch('/order/proceed/:id',async(req,res)=>{
-        console.log('patch patch')
+    app.patch('/order/proceed/:id',verifyJWT,verifyAdmin,async(req,res)=>{
         const id = req.params.id;
         const filter = {_id:ObjectId(id)};
         const updateDoc = {
@@ -116,12 +136,19 @@ async function run() {
         const result = await orderCollection.updateOne(filter,updateDoc);
         res.send(result);
     });
-    app.get('/orders',async(req,res)=>{
+    app.get('/orders',verifyJWT,verifyAdmin,async(req,res)=>{
         const cursor =  orderCollection.find({});
         const result = await cursor.toArray();
         res.send(result);
     });
-    app.post('/reviews',async(req,res)=>{
+    app.get('/orders/:email',verifyJWT,async(req,res)=>{
+        const email = req.params.email;
+        const query = {email:email};
+        const cursor = orderCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+    app.post('/reviews',verifyJWT,async(req,res)=>{
         const review = req.body;
         const result = await reviewCollection.insertOne(review);
         res.send(result);
@@ -132,21 +159,15 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
-    app.get('/orders/:email',async(req,res)=>{
-        const email = req.params.email;
-        const query = {email:email};
-        const cursor = orderCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    app.delete('/orders/:id',async(req,res)=>{
+   
+    app.delete('/orders/:id',verifyJWT,verifyAdmin,async(req,res)=>{
         const id = req.params.id;
         const filter = {_id : ObjectId(id)};
         const result = await orderCollection.deleteOne(filter);
         res.send(result);
 
     })
-    app.patch('/orders/:id',async(req,res)=>{
+    app.patch('/orders/:id',verifyJWT,async(req,res)=>{
         const id = req.params.id;
         const available = req.body.available;
         const filter = {_id:ObjectId(id)};
@@ -159,7 +180,7 @@ async function run() {
         res.send(result)
 
     })
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyJWT,verifyAdmin,async(req,res)=>{
         const cursor = userCollection.find({});
         const result = await cursor.toArray();
         res.send(result);
@@ -169,7 +190,6 @@ async function run() {
         const email = req.params.email;
         const filter = {email:email};
         const name = req.body;
-        console.log(name);
         const options = {upsert : true};
         const updateDoc = {
             $set:{
@@ -181,7 +201,7 @@ async function run() {
         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
         res.send({result,token});
     })
-    app.put("/user/admin/:email",async(req,res)=>{
+    app.put("/user/admin/:email",verifyJWT,verifyAdmin,async(req,res)=>{
         const email = req.params.email;
         const filter = {email : email};
         const option = {upsert:true}
@@ -193,7 +213,7 @@ async function run() {
         const result = await userCollection.updateOne(filter,updateDoc,option);
         res.send(result);
     })
-    app.patch('/users/update/:email',async(req,res)=>{
+    app.patch('/users/update/:email',verifyJWT,async(req,res)=>{
         const email = req.params.email;
         const filter = {email:email};
         const options = {upsert : true};
